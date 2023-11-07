fce915b34972dca0ca6cf6eb0d324dffe580ba85
Shane <shanec98@outlook.com>
2023-11-07 12:59:30 +0000

    Latest course work

---
+++ Section 6/components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>Hey!</p>
+
+  <Greeting :age="age"></Greeting>
+  <User :age="age" @age-change="updateAge"></User>
+</template>
+
+<script>
+import Greeting from "@/components/Greeting.vue"
+import User from "@/components/User.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 20
+    }
+  },
+  methods: {
+    updateAge(num) {
+      this.age += num
+    }
+  }
+}
+</script>
+
---
+++ Section 6/components/src/components/User.vue
@@ -0,0 +1,32 @@
+<template>
+    <button type="button" @click="onClickAge">Update Age</button>
+    <p>The user is {{ age }} years old</p>
+    <p>{{ ageDoubled }}</p>
+</template>
+
+<script>
+export default {
+    name: "User",
+    props: {
+        age: {
+            type: Number,
+            // required: true,
+            // default: 20
+            validator(value) {
+                return value < 130
+            }
+        }
+    },
+    emits: ['age-change'],
+    computed: {
+        ageDoubled() {
+            return this.age * 2
+        }
+    },
+    methods: {
+        onClickAge() {
+            this.$emit('age-change', 3)
+        }
+    }
+}
+</script>
