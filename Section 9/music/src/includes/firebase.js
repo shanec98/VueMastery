c32b5e4bc5a25fddef2b18193b5c6d69024328d4
Shane <shanec98@outlook.com>
2023-12-04 23:35:59 +0100

    file uploads and music player implementation

--- Section 9/music/src/includes/firebase.js
+++ Section 9/music/src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDER_5YLKBBiXsfiLY2M8rlUTBTip0gzdA',
@@ -14,7 +15,10 @@ firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 
 const usersCollection = db.collection('users')
+const songsCollection = db.collection('songs')
+const commentsCollection = db.collection('comments')
 
-export { auth, db, usersCollection }
+export { auth, db, usersCollection, storage, songsCollection, commentsCollection }
