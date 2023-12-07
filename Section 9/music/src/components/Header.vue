996e9c954242d003253411666591261f16b11bd1
Shane <shanec98@outlook.com>
2023-12-07 12:04:44 +0100

    Internationalisation and some optimisations

--- Section 9/music/src/components/Header.vue
+++ Section 9/music/src/components/Header.vue
@@ -31,6 +31,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -44,7 +51,10 @@ import useUserStore from '@/stores/user'
 export default {
   name: 'AppHeader',
   computed: {
-    ...mapStores(useModalStore, useUserStore)
+    ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === 'fr' ? 'French' : 'English'
+    }
   },
   methods: {
     toggleAuthModal() {
@@ -58,6 +68,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: 'home' })
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'fr' ? 'en' : 'fr'
     }
   }
 }
